f1222f13a9e3941c0a35fd38b7e25c3058925eb3
5jisoo <56earls@gmail.com>
2022-09-14 19:50:28 +0900

    basic settings
    initialize

---
+++ OurVillage/src/main/java/com/gjs/OurVillage/OurVillageApplication.java
@@ -0,0 +1,13 @@
+package com.gjs.OurVillage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurVillageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurVillageApplication.class, args);
+	}
+
+}
---
+++ OurVillage/src/test/java/com/gjs/OurVillage/OurVillageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gjs.OurVillage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurVillageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
